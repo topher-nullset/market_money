a7098fab269095aa0b40153bd977da53f65bd46b
Chris Cullinane <chriscullinane@protonmail.com>
2023-08-29 21:29:09 -0800

    serializers

---
+++ app/serializers/vendor_serializer.rb
@@ -0,0 +1,5 @@
+class VendorSerializer
+  include JSONAPI::Serializer
+  attributes :name, :description, :contact_name, :contact_phone, :credit_accepted
+  has_many :markets, through: :market_vendors
+end
