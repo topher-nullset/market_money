28d5d476f8bf4222306d02da46151cd4e8b5899c
Chris Cullinane <chriscullinane@protonmail.com>
2023-08-29 18:00:20 -0800

    show + sad path

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -6,6 +6,14 @@ module Api
         render json: @markets, methods: :vendor_count
       end
 
+      def show
+        @market = Market.find_by(id: params[:id])
+        if @market
+          render json: @market, methods: :vendor_count
+        else
+          render json: { errors: [{ detail: "Couldn't find Market with 'id'=#{params[:id]}" }] }, status: :not_found
+        end
+      end
     end
   end
 end
--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -16,4 +16,27 @@ RSpec.describe "Api::V0::Markets", type: :request do
       expect(first['vendor_count']).to eq(3)
     end
   end
+
+  describe 'GET /api/v0/markets/:id' do
+    it 'returns a JSON response with a market and vendor count' do
+      market = create(:market, :with_vendor)
+
+      get "/api/v0/markets/#{market.id}"
+      expect(response).to have_http_status(:success)
+
+      parsed = JSON.parse(response.body)
+      expect(parsed).to include('vendor_count')
+      expect(parsed['vendor_count']).to eq(3)
+    end
+
+    it 'returns a 404 error if the market does not exist' do
+      get "/api/v0/markets/1"
+      expect(response).to have_http_status(:not_found)
+
+      parsed = JSON.parse(response.body)
+      expect(parsed).to include('errors')
+      expect(parsed['errors'].first).to include('detail')
+      expect(parsed['errors'].first['detail']).to eq("Couldn't find Market with 'id'=1")
+    end
+  end
 end
